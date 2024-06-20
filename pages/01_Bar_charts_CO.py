0cc328d0632b6bd1530b18fc839bf9b541ffd23a
shashwatidc <64815163+shashwatidc@users.noreply.github.com>
2024-06-20 18:06:30 -0500

    Font fixing

--- pages/01_Bar_charts_CO.py
+++ pages/01_Bar_charts_CO.py
@@ -297,7 +297,7 @@ BIGGER_SIZE = 27 # set# trigger core fonts for PDF backend
 mp.rc('font', family = 'sans-serif') # 'Arial' # font group is sans-serif
 # st.write(plt.rcParams["font.sans-serif"][:]) # [  "DejaVu Sans",   "Bitstream Vera Sans",   "Computer Modern Sans Serif",   
 # "Lucida Grande",   "Verdana",   "Geneva",   "Lucid",   "Arial",   "Helvetica",   "Avant Garde",   "sans-serif"]
-plt.rcParams["font.sans-serif"] = ["Arial"]
+mp.rcParams["font.sans-serif"] = ["Arial"]
 mp.rc('font', size=MEDIUM_SIZE)     # controls default text sizes if unspecified
 mp.rc('axes', titlesize=MEDIUM_SIZE)    # fontsize of the axes title; I think this is for subplots 
 mp.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
