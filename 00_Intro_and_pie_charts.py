2e55ec1c57000a1e02dc3bbe9e33dd3f12b7d2d1
shashwatidc <64815163+shashwatidc@users.noreply.github.com>
2024-06-24 21:11:54 -0500

    Specialized different pie spacings

--- 00_Intro_and_pie_charts.py
+++ 00_Intro_and_pie_charts.py
@@ -1060,7 +1060,7 @@ if not np.isnan(FE_product_checked):
                     connectionstyle = f"angle,angleA=0,angleB={middle_angle}"
                     label_properties_away["arrowprops"].update({"connectionstyle": connectionstyle})
                     axs.annotate(df_opex.index[i], xy=(x_posn, y_posn), 
-                                 xytext=(far_near[flag]*np.sign(x_posn), 3.5*y_posn),
+                                 xytext=(far_near[flag]*np.sign(x_posn), 4.5*y_posn),
                                 horizontalalignment=horizontalalignment, 
                                 **label_properties_away)
                 else:                            
@@ -1118,7 +1118,7 @@ if not np.isnan(FE_product_checked):
                     connectionstyle = f"angle,angleA=0,angleB={middle_angle}"
                     label_properties_away["arrowprops"].update({"connectionstyle": connectionstyle})
                     axs.annotate(full_list_of_costs.index[i], xy=(x_posn, y_posn), 
-                                 xytext=(far_near[flag]*np.sign(x_posn), 3.5*y_posn),
+                                 xytext=(far_near[flag]*np.sign(x_posn), 4.5*y_posn),
                                 horizontalalignment=horizontalalignment, 
                                 **label_properties_away)
                 else:                            
@@ -1200,7 +1200,7 @@ if not np.isnan(FE_product_checked):
                         flag = -flag    
                         connectionstyle = f"angle,angleA=0,angleB={middle_angle}"
                         label_properties_away["arrowprops"].update({"connectionstyle": connectionstyle})
-                        axs.annotate(df_energy.iloc[2:-2].index[i], xy=(x_posn, y_posn), xytext=(far_near[flag]*np.sign(x_posn), 3.5*y_posn),
+                        axs.annotate(df_energy.iloc[2:-2].index[i], xy=(x_posn, y_posn), xytext=(far_near[flag]*0.5*np.sign(x_posn), 3.5*y_posn),
                                 horizontalalignment=horizontalalignment, 
                                 **label_properties_away)
                     else:                            
@@ -1252,7 +1252,7 @@ if not np.isnan(FE_product_checked):
                             connectionstyle = f"angle,angleA=0,angleB={middle_angle}"
                             label_properties_away["arrowprops"].update({"connectionstyle": connectionstyle})
                             axs.annotate(df_emissions.loc[~np.isnan(df_emissions)].iloc[:-2].index[i], xy=(x_posn, y_posn), 
-                                         xytext=(far_near[flag]*np.sign(x_posn), 3.5*y_posn),
+                                         xytext=(far_near[flag]*0.5*np.sign(x_posn), 3.5*y_posn),
                                         horizontalalignment=horizontalalignment, 
                                         **label_properties_away)
                         else:                            
