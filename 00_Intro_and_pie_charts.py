17b1307ea331f8d97843faaccdb2b9baedb3dec4
shashwatidc <64815163+shashwatidc@users.noreply.github.com>
2024-06-30 18:50:56 -0500

    Updated color schemes

--- 00_Intro_and_pie_charts.py
+++ 00_Intro_and_pie_charts.py
@@ -1034,24 +1034,20 @@ if not np.isnan(FE_product_checked):
     #___________________________________________________________________________________
 
     ############################### FIGURE OUTPUTS #####################################
-
+    
     ###### PIE CHART FORMATTING
-    ### Define colors for pie charts
+    flag = {True: 1,
+            False: 0}
+    ### Define colors
+    BM_capex_colors = [summer_r(i) for i in np.linspace(0, 1, len(df_capex_BM_vs_vbl.index)- flag[is_battery] - flag[is_additional_capex])] # battery gets its own color, so 1 less than capex length for other units
     if is_battery:
-        # Capex colors for bare modules
-        BM_capex_colors = [summer_r(i) for i in np.linspace(0, 1, len(df_capex_BM.index)-1)] # battery gets its own color, so 1 less than capex length for other units
-        BM_capex_colors.append((0.65, 0.65, 0.65, 1)) # add in battery 
-        if is_additional_capex:
-            BM_capex_colors.append((0.75, 0.75, 0.75, 1)) # add in battery 
-
-    else:
-        # Capex colors
-        BM_capex_colors = [summer_r(i) for i in np.linspace(0, 1, len(df_capex_BM.index))]
-        if is_additional_capex:
-            BM_capex_colors.append((0.75, 0.75, 0.75, 1)) # add in battery 
+        BM_capex_colors.append((0.65, 0.65, 0.65, 1)) # add in battery         
+    if is_additional_capex:
+        BM_capex_colors.append((0.75, 0.75, 0.75, 1)) # add in battery 
         
+    # Blues from 0.2 to 1 not bad but low contrast; YlGnbu not but looks jank with PuOr; winter_r is best
     # Opex colors
-    opex_colors = [PuOr(i) for i in np.linspace(0, 0.85, len(df_opex.index))]
+    opex_colors = [PuOr(i) for i in np.linspace(0, 0.85, len(df_opex_vs_vbl.index) - flag[is_additional_opex]) ]
     if is_additional_opex:
         opex_colors.append((0.75, 0.75, 0.75, 1)) # add in battery 
 
